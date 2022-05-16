01c525118da9b4ddd1f18b2a8d64fb6f18bc8655
IronBlood <aplshenyang@gmail.com>
2022-05-16 13:56:17 +0800

    新增 resolve_file 方法，将引用的 vue 组件路径转换为基于项目根路径的相对路径

--- src/file.ts
+++ src/file.ts
@@ -1,4 +1,7 @@
 import fs from "fs";
+import path from "path";
+
+const is_vue_file = (filename: string): boolean => /\.vue$/.test(filename);
 
 /**
  * Get all .vue files under a directory recursively.
@@ -9,10 +12,27 @@ export const walk_dir = (path: string, filelist: string[] = []) => {
 		const f_path = `${path}/${f}`;
 		if (fs.statSync(f_path).isDirectory()) {
 			walk_dir(f_path, filelist);
-		} else if (/\.vue$/.test(f)) {
+		} else if (is_vue_file(f_path)) {
 			filelist.push(f_path);
 		}
 	}
 	return filelist;
+}
+
+export interface ResolveFileOption {
+	filename: string;
+	basepath: string;
+}
+
+export const resolve_file = ({ filename, basepath }: ResolveFileOption): string | null => {
+	if (is_vue_file(filename)) {
+		if (/^@\//.test(filename))
+			return filename.replace("@", "./src");
+		if (/^\./.test(filename)) {
+			const fullpath = path.resolve(basepath, filename);
+			return fullpath.replace(path.resolve("."), ".");
+		}
+	}
+	return null;
 }
 
--- tests/file.test.ts
+++ tests/file.test.ts
@@ -1,4 +1,7 @@
-import { walk_dir } from "../src/file";
+import {
+	walk_dir,
+	resolve_file
+} from "../src/file";
 
 describe("test walk_dir", () => {
 	it("dummy dir", () => {
@@ -10,6 +13,29 @@ describe("test walk_dir", () => {
 			"./tests/.dummy_dir/b/b.vue",
 			"./tests/.dummy_dir/c/c/c.vue",
 		]));
+	});
+});
+
+describe("test resolve_file", () => {
+	test("@/views/a.vue", () => {
+		expect(resolve_file({
+			filename: "@/views/a.vue",
+			basepath: ".",
+		})).toEqual("./src/views/a.vue");
+	});
+
+	test("fs", () => {
+		expect(resolve_file({
+			filename: "fs",
+			basepath: ".",
+		})).toBeNull();
+	});
+
+	test("relative", () => {
+		expect(resolve_file({
+			filename: "./a.vue",
+			basepath: "./src/",
+		})).toEqual("./src/a.vue");
 	});
 });
 
